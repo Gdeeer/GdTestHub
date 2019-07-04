57266e23769aa77ccb55209c51502ba341bac3a5
zhangjl <zhangjl@2345.com>
2019-07-04 10:23:41 +0800

    页面跳转时的生命周期测试

---
+++ app/src/main/java/com/gdeer/gdtesthub/jump/JumpActivity.java
@@ -0,0 +1,68 @@
+package com.gdeer.gdtesthub.jump;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.gdeer.gdtesthub.R;
+import com.gdeer.gdtesthub.databinding.ActivityJumpBinding;
+
+public class JumpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("zhangjl", "jump onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jump);
+
+        ActivityJumpBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_jump);
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JumpActivity.this, TargetActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JumpActivity.this, TargetActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("zhangjl", "jump onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("zhangjl", "jump onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("zhangjl", "jump onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("zhangjl", "jump onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("zhangjl", "jump onDestroy");
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/gdeer/gdtesthub/jump/TargetActivity.kt
@@ -0,0 +1,43 @@
+package com.gdeer.gdtesthub.jump
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.gdeer.gdtesthub.R
+import kotlinx.android.synthetic.main.activity_target.*
+
+class TargetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("zhangjl", "target onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_target)
+
+        webview.loadUrl("https://blog.csdn.net/gdeer")
+    }
+
+    override fun onStart() {
+        Log.d("zhangjl", "target onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("zhangjl", "target onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("zhangjl", "target onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("zhangjl", "target onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("zhangjl", "target onDestroy")
+        super.onDestroy()
+    }
+}
