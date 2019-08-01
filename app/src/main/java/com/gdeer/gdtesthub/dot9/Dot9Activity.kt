2cdb50fc2a87041de879bf3e9fdf63b9d52b5202
zhangjl <zhangjl@2345.com>
2019-08-01 13:14:21 +0800

    .9 图的使用 test

---
+++ app/src/main/java/com/gdeer/gdtesthub/dot9/Dot9Activity.kt
@@ -0,0 +1,13 @@
+package com.gdeer.gdtesthub.dot9
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gdeer.gdtesthub.R
+
+class Dot9Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dot9)
+    }
+}
