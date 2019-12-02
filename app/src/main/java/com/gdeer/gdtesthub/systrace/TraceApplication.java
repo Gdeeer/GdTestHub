9ec03bf49d32b2c33a3249cc56092b0f17f59199
gdeer <gdeer00@163.com>
2019-12-02 17:42:36 +0800

    启动时长统计，Application onCreate、attachBaseContext 的记录

--- app/src/main/java/com/gdeer/gdtesthub/systrace/TraceApplication.java
+++ app/src/main/java/com/gdeer/gdtesthub/systrace/TraceApplication.java
@@ -1,13 +1,22 @@
 package com.gdeer.gdtesthub.systrace;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.Trace;
+import android.util.Log;
 
 public class TraceApplication extends Application {
     @Override
     public void onCreate() {
+        Log.d("zhangjl", "application onCreate");
         Trace.beginSection("gdeer-Application");
         super.onCreate();
         Trace.endSection();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        Log.d("zhangjl", "attachBaseContext");
+        super.attachBaseContext(base);
     }
 }
