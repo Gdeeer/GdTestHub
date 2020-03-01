ff9f169c2c31eba2728d9f2e0a63a82fa06f80d2
gdeeer <gdeer00@163.com>
2020-03-01 17:33:38 +0800

    【MotionLayout】MotionLayout 的简单使用

---
+++ app/src/main/java/com/gdeer/gdtesthub/constraintLayout/MotionLayoutActivity.kt
@@ -0,0 +1,14 @@
+package com.gdeer.gdtesthub.constraintLayout
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gdeer.gdtesthub.R
+
+class MotionLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+//        setContentView(R.layout.activity_motion_layout_img_filter_view)
+    }
+}
