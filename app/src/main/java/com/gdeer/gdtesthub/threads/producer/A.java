d48a600fe08545096f1398d699707abf46377c16
gdeeer <gdeer00@163.com>
2020-03-21 17:22:31 +0800

    【多线程】生产者消费者模型

---
+++ app/src/main/java/com/gdeer/gdtesthub/threads/producer/A.java
@@ -0,0 +1,67 @@
+package com.gdeer.gdtesthub.threads.producer;
+
+import com.gdeer.gdtesthub.rxjava.ThreadUtil;
+
+/**
+ * 生产者消费者模型
+ */
+public class A {
+    private static int count;
+    private static final int MAX = 10;
+    private static Object lock = "lock";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            Producer producer = new Producer();
+            producer.produce();
+        }
+        for (int i = 0; i < 3; i++) {
+            Consumer consumer = new Consumer();
+            consumer.consume();
+        }
+    }
+
+    static class Producer {
+        public void produce() {
+            new Thread(() -> {
+                while (true) {
+                    ThreadUtil.sleep(200);
+                    synchronized (lock) {
+                        while (count >= MAX) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        count++;
+                        System.out.println("produceTo: " + count);
+                        lock.notifyAll();
+                    }
+                }
+            }).start();
+        }
+    }
+
+    static class Consumer {
+        public void consume() {
+            new Thread(() -> {
+                while (true) {
+                    ThreadUtil.sleep(200);
+                    synchronized (lock) {
+                        while (count <= 0) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        count--;
+                        System.out.println("consumeTo: " + count);
+                        lock.notifyAll();
+                    }
+                }
+            }).start();
+        }
+    }
+}
