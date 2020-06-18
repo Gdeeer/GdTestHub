28616d102f8ab37ec558056ad75dc1443e91939e
gdeer <gdeer00@163.com>
2020-06-18 10:16:47 +0800

    【room】room 警告修改

--- app/src/main/java/com/gdeer/gdtesthub/db/coverupgradetest/UsaDatabase.java
+++ app/src/main/java/com/gdeer/gdtesthub/db/coverupgradetest/UsaDatabase.java
@@ -3,7 +3,7 @@ package com.gdeer.gdtesthub.db.coverupgradetest;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {UsaCity.class}, version = 3)
+@Database(entities = {UsaCity.class}, version = 3, exportSchema = false)
 public abstract class UsaDatabase extends RoomDatabase {
     public abstract UsaCityDao cityDao();
 }
--- app/src/main/java/com/gdeer/gdtesthub/db/room/ChinaDatabase.java
+++ app/src/main/java/com/gdeer/gdtesthub/db/room/ChinaDatabase.java
@@ -3,7 +3,7 @@ package com.gdeer.gdtesthub.db.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {City.class}, version = 2)
+@Database(entities = {City.class}, version = 2, exportSchema = false)
 public abstract class ChinaDatabase extends RoomDatabase {
     public abstract CityDao cityDao();
 }
