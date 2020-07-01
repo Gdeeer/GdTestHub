19189063e0a7b59d99c0b9b74e0ceca9d95a0c15
gdeer <gdeer00@163.com>
2020-07-01 17:40:37 +0800

    【Drawable】drawable-nodpi 目录测试

--- app/src/main/java/com/gdeer/gdtesthub/drawable/DrawableActivity.kt
+++ app/src/main/java/com/gdeer/gdtesthub/drawable/DrawableActivity.kt
@@ -6,6 +6,12 @@ import com.gdeer.gdtesthub.R
 import com.gdeer.gdtesthub.utils.DeviceUtil
 import kotlinx.android.synthetic.main.activity_drawable.*
 
+/**
+ * 1080 宽的图，加载到 1080p 的手机上，结果并不是 1080
+ * 因为图放在 xxhdpi 目录下，会根据 dpi 缩放
+ * 如果放在 nodpi 目录下，就会显示 1080 的像素
+ * 如果放在 drawable 目录下，会按屏幕宽高去加载
+ */
 class DrawableActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
