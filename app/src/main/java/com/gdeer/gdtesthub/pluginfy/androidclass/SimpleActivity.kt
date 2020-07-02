276f0a0be3741e69805b921c59296f3c8a17e05a
gdeer <gdeer00@163.com>
2020-07-02 17:25:01 +0800

    【优化】重命名

--- app/src/main/java/com/gdeer/gdtesthub/pluginfy/androidclass/SimpleActivity.kt
+++ app/src/main/java/com/gdeer/gdtesthub/pluginfy/androidclass/SimpleActivity.kt
@@ -4,16 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gdeer.gdtesthub.R
-import kotlinx.android.synthetic.main.activity_common.*
+import kotlinx.android.synthetic.main.activity_simple.*
 
 /**
  * 普通 Activity，点击启动未注册的 targetActivity
  */
-class CommonActivity : AppCompatActivity() {
+class SimpleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_common)
+        setContentView(R.layout.activity_simple)
 
         btn_start_target.setOnClickListener {
             startActivity(Intent(this, TargetActivity::class.java))
