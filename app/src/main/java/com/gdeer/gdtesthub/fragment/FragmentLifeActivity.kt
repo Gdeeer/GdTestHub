c0e9c1ab37e5c4b186122087f3cf61681128488a
gdeer <gdeer00@163.com>
2020-08-26 16:02:31 +0800

    【Fragment】add 的 Fragment 异常恢复

--- app/src/main/java/com/gdeer/gdtesthub/fragment/FragmentLifeActivity.kt
+++ app/src/main/java/com/gdeer/gdtesthub/fragment/FragmentLifeActivity.kt
@@ -6,21 +6,27 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.gdeer.gdtesthub.R
 import com.gdeer.gdtesthub.fragment.ui.fragmentFirst.FirstFragment
-import kotlinx.android.synthetic.main.fragment_life_activity.*
 
 class FragmentLifeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("zhangjl", "FragmentLifeActivity onCreate beforeSuper $savedInstanceState ${supportFragmentManager.fragments.size}")
+        Log.d("zhangjl", "FragmentLifeActivity onCreate beforeSuper $savedInstanceState ${supportFragmentManager.fragments.size} " + this.hashCode())
         super.onCreate(savedInstanceState)
         Log.d("zhangjl", "FragmentLifeActivity onCreate afterSuper $savedInstanceState ${supportFragmentManager.fragments.size}")
         setContentView(R.layout.fragment_life_activity)
 
-        frag_container.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.frag_container, FirstFragment.newInstance())
-                    .commitNow()
-        }
+        // 在 onCreate 中 add 一个 Fragment，再杀死 Activity，再恢复，会有两个 Fragment
+        // 一个是在 super.onCreate 中恢复的 Fragment，一个是在后面又 add 的 Fragment
+        // replace 也一样，会有两个，不过后面的会覆盖掉前面的
+
+//        frag_container.setOnClickListener {
+        supportFragmentManager.beginTransaction()
+                .addToBackStack("a")
+                .add(R.id.frag_container, FirstFragment.newInstance())
+                .commit()
+//        }
+
+        Log.d("zhangjl", "fragments afterCommit fragments size: ${supportFragmentManager.fragments.size}")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
--- app/src/main/java/com/gdeer/gdtesthub/fragment/ui/fragmentFirst/FirstFragment.kt
+++ app/src/main/java/com/gdeer/gdtesthub/fragment/ui/fragmentFirst/FirstFragment.kt
@@ -21,23 +21,23 @@ class FirstFragment : Fragment() {
     private lateinit var mViewModel: FirstViewModel
 
     override fun onAttach(context: Context) {
-        Log.d("zhangjl", "first onAttach")
+        Log.d("zhangjl", "first onAttach " + this.hashCode())
         super.onAttach(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("zhangjl", "first onCreate")
+        Log.d("zhangjl", "first onCreate " + this.hashCode())
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        Log.d("zhangjl", "first onCreateView")
+        Log.d("zhangjl", "first onCreateView " + this.hashCode())
         return inflater.inflate(R.layout.fragment_life_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d("zhangjl", "first onActivityCreated")
+        Log.d("zhangjl", "first onActivityCreated " + this.hashCode())
         super.onActivityCreated(savedInstanceState)
         mViewModel = ViewModelProviders.of(this).get(FirstViewModel::class.java)
         // TODO: Use the ViewModel
@@ -50,42 +50,42 @@ class FirstFragment : Fragment() {
     }
 
     override fun onStart() {
-        Log.d("zhangjl", "first onStart")
+        Log.d("zhangjl", "first onStart " + this.hashCode())
         super.onStart()
     }
 
     override fun onResume() {
-        Log.d("zhangjl", "first onResume")
+        Log.d("zhangjl", "first onResume " + this.hashCode())
         super.onResume()
     }
 
     override fun onPause() {
-        Log.d("zhangjl", "first onPause")
+        Log.d("zhangjl", "first onPause " + this.hashCode())
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d("zhangjl", "first onStop")
+        Log.d("zhangjl", "first onStop " + this.hashCode())
         super.onStop()
     }
 
     override fun onDestroyView() {
-        Log.d("zhangjl", "first onDestroyView")
+        Log.d("zhangjl", "first onDestroyView " + this.hashCode())
         super.onDestroyView()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("zhangjl", "first onSaveInstanceState")
+        Log.d("zhangjl", "first onSaveInstanceState " + this.hashCode())
         super.onSaveInstanceState(outState)
     }
 
     override fun onDestroy() {
-        Log.d("zhangjl", "first onDestroy")
+        Log.d("zhangjl", "first onDestroy " + this.hashCode())
         super.onDestroy()
     }
 
     override fun onDetach() {
-        Log.d("zhangjl", "first onDetach")
+        Log.d("zhangjl", "first onDetach " + this.hashCode())
         super.onDetach()
     }
 }
