94eb9e95096640d122887e7bb04a4f23bcd82b47
gdeer <gdeer00@163.com>
2020-09-15 16:22:02 +0800

    【Shadow】elevation 不生效

---
+++ app/src/main/java/com/gdeer/gdtesthub/view/shadow/ShadowFragment.kt
@@ -0,0 +1,10 @@
+package com.gdeer.gdtesthub.view.shadow
+
+import com.gdeer.gdtesthub.BaseFragment
+import com.gdeer.gdtesthub.R
+
+class ShadowFragment: BaseFragment() {
+    override fun getLayout(): Int {
+        return R.layout.fragment_shadow
+    }
+}
