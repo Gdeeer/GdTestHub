ad9a16fde2d23cd98cc7ccdae157bcf5527efc0b
gdeer <gdeer00@163.com>
2020-09-27 10:39:04 +0800

    【ScrollView】fillViewport 测试

---
+++ app/src/main/java/com/gdeer/gdtesthub/view/scrollview/ScrollViewFragment.kt
@@ -0,0 +1,10 @@
+package com.gdeer.gdtesthub.view.scrollview
+
+import com.gdeer.gdtesthub.BaseFragment
+import com.gdeer.gdtesthub.R
+
+class ScrollViewFragment: BaseFragment() {
+    override fun getLayout(): Int {
+        return R.layout.fragment_scrollview
+    }
+}
